93546a53aab37b3b86aa8da9f50356b145417ef2
Vincent Jang <vincentjangdev@gmail.com>
2024-10-06 20:27:26 +0900

    fix : chore

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -8,26 +8,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
+  HelloController() {
+    System.out.println("HelloController() called");
+  }
 
   @GetMapping("/hello")
-  public String hello(Model model) { // Spring will inject the Model object into the method
-    model.addAttribute("data", "hello!!");
+  public String hello(Model model, @RequestParam("name") String name) { // Spring will inject the Model object into the
+                                                                        // method
+    model.addAttribute("data", "hello!! " + name);
 
     // resources/templates/hello.html will be rendered
     // ViewResolver will resolve the view name to the template file
     return "hello";
   }
 
-  @GetMapping("/hello-mvc")
-  @ResponseBody
-  public String helloMvc(@RequestParam("name") String name, Model model) {
-    // model.addAttribute("name", name + "!");
-
-    return "hello " + name + "!";
-  }
-
   @GetMapping("/hello-api")
-  @ResponseBody
+  @ResponseBody // Don't use ViewResolver, just return the object as JSON
   // HttpMessageConvertor will convert the object to JSON, (with StringConverter
   // and JsonConverter)
   public Hello helloApi(@RequestParam("name") String name) {
