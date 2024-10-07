84bbcc6579a0ac08ef44ff64956dd40e8f63ccf4
Vincent Jang <vincentjangdev@gmail.com>
2024-10-08 01:05:24 +0900

    test : add commend and findAll test case

--- src/test/java/com/example/demo/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/com/example/demo/repository/MemoryMemberRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.example.demo.repository;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import com.example.demo.domain.Member;
@@ -8,6 +9,15 @@ import com.example.repository.MemoryMemberRepository;
 
 class MemoryMemberRepositoryTest {
   MemoryMemberRepository repository = new MemoryMemberRepository();
+
+  @AfterEach
+  public void afterEach() {
+    // 여기서 clearStore를 호출하지 않으면 테스트가 끝나고 다음 테스트를 실행할 때 store에 데이터가 남아있어서 문제가 발생할 수
+    // 있다.
+    // 테스트 함수의 순서는 보장되지 않기 때문에.
+    // 테스트는 서로 의존관계가 없어야 한다
+    repository.clearStore();
+  }
 
   @Test
   public void save() {
@@ -35,6 +45,19 @@ class MemoryMemberRepositoryTest {
     Member result = repository.findByName("spring1").get();
 
     Assertions.assertThat(result).isEqualTo(member1);
+  }
+
+  @Test
+  public void findAll() {
+    Member member1 = new Member();
+    member1.setName("spring1");
+    repository.save(member1);
+
+    Member member2 = new Member();
+    member2.setName("spring2");
+    repository.save(member2);
+
+    Assertions.assertThat(repository.findAll().size()).isEqualTo(2);
   }
 
 }
